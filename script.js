40649325eb339d26d8f4f63e33a31eba98fd4788
airart <62560744+airart@users.noreply.github.com>
2026-09-25 16:37:44 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -82,7 +82,7 @@ async function askAI(question){
     try{
 
         const response = await fetch(
-        `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${API_KEY}`,
+        `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${API_KEY}`,
         {
             method:"POST",
             headers:{
